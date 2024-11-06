86641bd142533ffdfaad0097152451d739f91943
Enrico Gazoth <enrigazoth@gmail.com>
2024-11-06 11:27:20 -0300

    Obtendo um usuário por id

--- src/main/java/com/enrico/workshopmongo/services/UserService.java
+++ src/main/java/com/enrico/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.enrico.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.enrico.workshopmongo.domain.User;
 import com.enrico.workshopmongo.repository.UserRepository;
+import com.enrico.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -18,5 +20,8 @@ public class UserService {
 		return repo.findAll();
 	}
 	
-	
+	public User findById(String id) {
+		Optional<User> user = repo.findById(id);
+		return user.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+	}
 }
