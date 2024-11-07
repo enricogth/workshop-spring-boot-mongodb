fc384c4a9b139bf305adecd6a32f65169f11b175
Enrico Gazoth <enrigazoth@gmail.com>
2024-11-07 18:02:11 -0300

    Inserção de usuário com POST

--- src/main/java/com/enrico/workshopmongo/services/UserService.java
+++ src/main/java/com/enrico/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.enrico.workshopmongo.domain.User;
+import com.enrico.workshopmongo.dto.UserDTO;
 import com.enrico.workshopmongo.repository.UserRepository;
 import com.enrico.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -24,4 +25,13 @@ public class UserService {
 		Optional<User> user = repo.findById(id);
 		return user.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
 	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+	}
+	
 }
